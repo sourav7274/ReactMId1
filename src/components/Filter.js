e276680eef06ca603964360748927c1044e0e2d4
sourav7274 <55195415+sourav7274@users.noreply.github.com>
2023-09-14 22:52:16 +0530

    Add files via upload

---
+++ src/components/Filter.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const Filter = (props) => {
+  let filterData = props.filterData;
+  let category = props.category;
+  let setCategory = props.setCategory;
+
+  function filterHandler(title){
+    setCategory(title);
+  }
+  return (
+    <div className='w-11/12 flex flex-wrap max-w-max space-x-4 gap-y-4 mx-auto py-4 justify-center'>
+      {
+        filterData.map( (data) => (
+          <button className={`text-lg-2 px-1 rounded-md font-medium
+          text-white bg-black hover:bg-opacity-50 border-2  transition-all duration-300
+          ${category === data.title ? "bg-opcaity-60 border-white"
+          : "bg-opcaity-40 border-transparent"
+          }
+          `}
+          key={data.id}
+          onClick={() => filterHandler(data.title)}
+          >{data.title}</button>
+        ))
+      }
+    </div>
+  )
+ }
+    
+
+export default Filter
